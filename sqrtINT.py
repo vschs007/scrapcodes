448a248df77a8662fff12839ca0cf9b4109340ed
vivek <vschs007@gmail.com>
2020-06-27 11:12:21 +0530

    2240 ms leetcode updated

--- sqrtINT.py
+++ sqrtINT.py
@@ -10,4 +10,25 @@ print(i-1)
 
 
 
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if (x < 0):
+            return False
+        if (x == 0 or x == 1):
+            return x
+        res = 1
+        i = 1
+        while (res <= x):
+            i += 1
+            res = i * i
+        return i - 1
+
+
+
+
+
+
+
+
+
 
