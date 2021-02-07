6048d3f881aef33b62043da32215c8e312d5f369
vivek <vschs007@gmail.com>
2021-02-07 10:55:11 +0530

    vaid parentheses

---
+++ valid_parentheses.py
@@ -0,0 +1,33 @@
+s = "}}}}}}}}}}}}}}"
+print(len(s))
+arr=[]
+for i in range(len(s)):
+    if(s[i]=="(" or s[i]=="{" or s[i]=="["):
+        arr.append(s[i])
+    if (s[i] == ")"):
+        if(len(arr)==0 or arr[-1]!="("):
+            arr.append(s[i])
+        elif(arr[-1]=="("):
+            arr.pop()
+
+
+
+    if (s[i] == "}"):
+        if (len(arr) == 0 or arr[-1]!="{"):
+            arr.append(s[i])
+        elif (arr[-1] == "{"):
+            arr.pop()
+
+
+    if (s[i] == "]"):
+        if (len(arr) == 0 or arr[-1]!="["):
+            arr.append(s[i])
+        elif (arr[-1] == "["):
+            arr.pop()
+    print(arr)
+
+print(arr)
+if(len(arr)==0):
+    print("True")
+else:
+    print("False")
