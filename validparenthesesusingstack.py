3e16e0396c2dd8c358aa04373c392b0cad4771de
vivek <vschs007@gmail.com>
2021-06-30 10:47:15 +0530

    balanced parentheses using stack

---
+++ validparenthesesusingstack.py
@@ -0,0 +1,17 @@
+s="((({{}})))"
+n=len(s)
+arr=[len(s)]
+if(s[0] in ['}',']',')']):
+    print("unbalanced")
+else:
+    arr[0]=s[0]
+for i in range(1,n):
+    if s[i] in ['{','(','[']:
+        arr.append(s[i])
+    elif s[i] in ['}',']',')'] and len(arr)>0:
+        arr.pop()
+
+if len(arr) ==0:
+    print("balanced")
+else:
+    print("unbalanced")
