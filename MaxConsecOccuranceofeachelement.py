4712941618d22dac126db96def2366cc7023f520
Vivek Kumar Singh <vschs007@gmail.com>
2021-08-25 21:08:52 +0530

    Create MaxConsecOccuranceofeachelement.py

---
+++ MaxConsecOccuranceofeachelement.py
@@ -0,0 +1,17 @@
+strr = "aabaaabbccccczzxzzzziicccccccccc"
+temp = list(set(strr))
+count = 0
+maxcount =0
+res=[]
+for i in range(len(temp)):
+    for j in range(len(strr)):
+        if strr[j]==temp[i]:
+            count+=1
+        else:
+            maxcount = max(maxcount,count)
+            count = 0
+
+    res.append([temp[i],max(count,maxcount)])
+    maxcount = 0
+
+print(res)
