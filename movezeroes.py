8450a84b91ec6307c28bf99b09239820b1c54221
Vivek Kumar Singh <vschs007@gmail.com>
2021-12-21 21:00:40 +0530

    move zeroes leetcode 98% faster

---
+++ movezeroes.py
@@ -0,0 +1,15 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        counzero = 0
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                counzero += 1
+        s = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[s] = nums[i]
+                s += 1
+        for i in range(len(nums) - 1, len(nums) - counzero - 1, -1):
+            nums[i] = 0
+
+        return nums
