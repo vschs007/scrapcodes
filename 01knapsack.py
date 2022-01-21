5b7079ddafc8e4d2adbc2c1be0390d74e9933130
Vivek Kumar Singh <vschs007@gmail.com>
2022-01-21 10:43:57 +0530

    01 knapsack memoization

--- 01knapsack.py
+++ 01knapsack.py
@@ -1,16 +1,23 @@
+
 def knapsack(w,c,capacity,n):
     if n==0 or capacity==0:
         return 0
+    elif t[n][n]!=-1:
+        return t[n][n]
     elif w[n-1]<=capacity:
-        return max(c[n-1]+knapsack(w,c,capacity-w[n-1],n-1),knapsack(w,c,capacity,n-1))
+      #  return  max(c[n-1]+knapsack(w,c,capacity-w[n-1],n-1),knapsack(w,c,capacity,n-1))
+        t[n][n]= max(c[n-1]+knapsack(w,c,capacity-w[n-1],n-1),knapsack(w,c,capacity,n-1))
     else:
-        return knapsack(w,c,capacity,n-1)
+        #return knapsack(w,c,capacity,n-1)
+        t[n][n]= knapsack(w,c,capacity,n-1)
+    return t[n][n]
 
-weight=[2,4,6,8,2,4,8]
-cost=[4,6,2,3,1,8,7]
-capacity = 10
-
-n= len(weight)
+weight=[20, 10, 40, 30]
+cost=[40, 100, 50, 60]
+capacity = 60
+n=len(weight)
+global t
+t=[[-1]*(n+1)]*(n+1)
 print(knapsack(weight,cost,capacity,n))
 
 
