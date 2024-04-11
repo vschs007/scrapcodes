814208209b2487104d567e1e655b478bd0711523
Vivek Kumar Singh <vschs007@gmail.com>
2024-04-11 12:23:08 +0530

    jump game leetcode

--- jumpgame.py
+++ jumpgame.py
@@ -1,12 +1,1 @@
-N =4
-allpair =[]
-a = 1
-count=0
-while(a<N):
-    allpair.append([a,N-a])
-    a+=1
-
-for i in range(len(allpair)):
-    if allpair[i][0]+allpair[i][1] == 2* ((allpair[i][0])^(allpair[i][1])):
-        count+=1
-print(count)
+nums = [2,3,1,1,4]
